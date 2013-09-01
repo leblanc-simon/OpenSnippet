b4cb383891e5eafc3c796513f988cdf331095b0c
Simon Leblanc <contact@leblanc-simon.eu>
2013-09-02 01:59:17 +0200

    Select code

--- web/js/app.js
+++ web/js/app.js
@@ -1,3 +1,21 @@
+jQuery.fn.selectText = function(){
+    var doc = document
+        , element = this[0]
+        , range, selection
+    ;
+    if (doc.body.createTextRange) {
+        range = document.body.createTextRange();
+        range.moveToElementText(element);
+        range.select();
+    } else if (window.getSelection) {
+        selection = window.getSelection();        
+        range = document.createRange();
+        range.selectNodeContents(element);
+        selection.removeAllRanges();
+        selection.addRange(range);
+    }
+};
+
 $(function() {
     // Visual effect
     $(".toggle-sidebar").click(function () {
@@ -7,4 +25,9 @@ $(function() {
     $(".extends").click(function(){
         $(this).parents('section').find('.code').toggleClass('not-all');
     });
+
+    $(".copy").click(function(){
+       $(this).parents('section').find('.code').removeClass('not-all');
+       $(this).parents('section').find('.code pre').selectText();
+    })
 });
